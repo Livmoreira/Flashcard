bc2b43e776174651a6be0fc4bb040d76a333bf5b
Livmoreira <00001077885313sp@al.educacao.sp.gov.br>
2024-11-28 01:19:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Biologia',
+    'Até que idade os ossos crescem',
+    '18 Anos'
+)
+
+criaCartao(
+    'Fisica',
+    'Qual é a formula de Delta ',
+    'Δ = b2 – 4ac'
+)
+
+criaCartao(
+    'Português',
+    'Que autor Iniciou o romantismo no brasil?',
+    'Gonçalves de Magalhães'
+)
+
+criaCartao(
+    'matematica',
+    'raiz quadrade de 1500?',
+    '38.73'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Alemanha?',
+    'Berlim'
+)
+
+criaCartao(
+    'Inglês',
+    'What is the biggest animal in the world?', 
+    'blue whale'
+)
